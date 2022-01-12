5bfd6e21e9fcfc5ee6fb27cc126cf1fbb8c5cbed
sbasaranoglu <suleyman.basaranoglu@hepsiburada.com>
2022-01-12 09:37:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/roomstructure/entities/RoomEntities.kt
@@ -0,0 +1,19 @@
+package com.example.roomstructure.entities
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "aliens_table")
+data class RoomEntities(
+    @ColumnInfo(name = "alienName")
+    var alienName: String? = null,
+    @ColumnInfo(name = "alienRegion")
+    var alienRegion: String? = null,
+    @ColumnInfo(name = "alienAge")
+    var alienAge: Int? = null,
+    @ColumnInfo(name = "alienImage")
+    var alienImage: String? = null,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+)
